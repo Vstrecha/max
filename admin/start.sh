43f646c87987c07f1d9a5c5ef5cb36f8cda56110
user <feda.avdeev@yandex.ru>
2025-11-08 14:40:31 +0300

    fix: rename containers & change .env.example

--- admin/start.sh
+++ admin/start.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Waiting for database to be ready..."
-while ! python -c "import psycopg2; psycopg2.connect(dbname='${DB_NAME:-vstrecha}', user='${DB_USER:-postgres}', password='${DB_PASSWORD:-postgres}', host='${DB_HOST:-db}', port='${DB_PORT:-5432}')" 2>/dev/null; do
+while ! python -c "import psycopg2; psycopg2.connect(dbname='${DB_NAME}', user='${DB_USER}', password='${DB_PASSWORD}', host='${DB_HOST}', port='${DB_PORT}')" 2>/dev/null; do
     echo "Database is unavailable - sleeping"
     sleep 1
 done
