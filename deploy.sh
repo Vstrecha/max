97a6c082a8136ebd677604b8413884ab7ade29eb
user <feda.avdeev@yandex.ru>
2025-11-08 13:53:01 +0300

    add: deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+cd "$HOME/max"
+
+echo "🔄 Pulling latest code"
+git reset --hard origin/main && git pull
+
+echo "🚀 Starting services"
+docker compose down --remove-orphans
+docker compose up --build -d
