907c657d5a2e47e5c88399b3d926adda3a9f8238
user <feda.avdeev@yandex.ru>
2025-11-08 13:45:44 +0300

    add: nginx to docker-compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,9 +5,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
-    base: command === 'serve' ? '/xaid5ooPa0eepui/' : '/',
+    base:  '/',
     plugins: [vue()],
     server: {
       allowedHosts: ['horseshoe-crab.ru', 'localhost', 'host.docker.internal'],
