f9229f454c6ec34e7250dcc4df79fa738d2bf936
user <feda.avdeev@yandex.ru>
2025-11-09 16:48:51 +0300

    hotfix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, ViteDevServer } from 'vite'
 
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import morgan from 'morgan'
 
 // https://vite.dev/config/
 export default defineConfig(() => {
   return {
-    plugins: [morganPlugin(), vue(), vueDevTools()],
+    plugins: [morganPlugin(), vue()],
     server: {
       allowedHosts: ['horseshoe-crab.ru', 'localhost', 'max-total.ru'],
     },
