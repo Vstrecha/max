d029b5440400d2ac1aa5838ba5ea562fca26541c
user <feda.avdeev@yandex.ru>
2025-11-15 00:00:35 +0300

    ашчЖ ьфч

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-cd "$HOME/max-events"
+cd "$HOME/max"
 
 echo "🔄 Pulling latest code"
 git reset --hard origin/main && git pull
 
 echo "🚀 Starting services"
 docker compose down --remove-orphans
-docker volume rm max-events_frontend-dist
+docker volume rm max_frontend-dist
 docker compose up --build -d
